52cb9299e373e8b916315521b11c90bb27532f07
Misha <zorin.mikhail.e@gmail.com>
2025-12-11 22:59:30 +0300

    xz

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ let find = document.getElementById('find')
 
 regions.forEach(region => {
     let randomRegion = Math.floor(Math.random() * regions.length)
-    console.log(randomRegion.id);
-    find.innerHTML = "Найди на карте:" + randomRegion.id
+    // Получаем регион по индексу
+    let randomIndex = regions[randomRegion]
+    // console.log(randomRegion);
+    find.innerHTML = "Найди на карте:" + randomIndex.id
     // console.log(region);
     region.addEventListener('click', function () {
         // alert("Регион:" + region.id)
+        console.log(region.id);
+        
         if (randomRegion.id === region.id) {
             find.innerHTML = 'Победа!'
             this.style.fill = 'green'
